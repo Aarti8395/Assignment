b590035e565b2d0677d2b35ccc80533259b21216
Aarti8395 <aartiy8395@gmail.com>
2025-05-23 15:27:23 +0530

    2 task

---
+++ Assignment/Fibonacci Series/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Enter number of terms ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int first = 0, second = 1, next;
+Console.Write("Fibonacci series: ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(first + " ");
+            
+    next = first + second;
+    first = second;
+    second = next;
+}
+Console.ReadKey();
